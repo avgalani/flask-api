7f67ab6441b2b91ea73c7e8a21e2d8f73dfacf63
root <root@galactionn.localdomain>
2018-07-05 14:51:17 +0200

    added roles endpoint

--- App/controller.py
+++ App/controller.py
@@ -2,7 +2,7 @@ __author__ = 'responsible'
 from flask_restful import Resource, reqparse
 from flask_security import auth_token_required, roles_required, login_user
 from passlib.handlers.django import django_pbkdf2_sha256
-from .models import User, Service
+from .models import User, Service, Role
 from App import ma, jsonify, request, db
 
 
@@ -11,37 +11,31 @@ class UserSchema(ma.Schema):
         # Fields to expose
         fields = ('username', 'password')
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
 class ServiceSchema(ma.Schema):
     class Meta:
         fields = ('name','properties')
 
+class RoleSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'description')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 service_schema = ServiceSchema()
 services_schema = ServiceSchema(many=True)
-
-class Protected(Resource):
-    @auth_token_required
-    def get(self):
-        return {"msg": "pl"}, 200
-
-    @auth_token_required
-    @roles_required('admin')  
-    def post(self):
-        return {"msg": "okay"}, 201
-
+role_schema = RoleSchema()
+roles_schema = RoleSchema(many=True)
 
 class Login(Resource): 
     def post(self):
         args = reqparse.RequestParser() \
-            .add_argument('username', type=str, location='json', required=True, help="hh") \
-            .add_argument("password", type=str, location='json', required=True, help="ohk") \
+            .add_argument('username', type=str, location='json', required=True) \
+            .add_argument("password", type=str, location='json', required=True) \
             .parse_args()
         user = User.authenticate(args['username'], args['password'])
         if user:
             login_user(user=user)
-            return {"message": "okk", "token": user.get_auth_token()}, 200
+            return {"message": "login succeeded", "token": user.get_auth_token()}, 200
         else:
             return {"message": "nok"}, 401
 
@@ -73,4 +67,10 @@ class Services(Resource):
     def get(self):
         all_services = Service.query.all()
         result = services_schema.dump(all_services)
+        return jsonify(result.data)
+
+class Roles(Resource):
+    def get(self):
+        all_roles = Role.query.all()
+        result = roles_schema.dump(all_roles)
         return jsonify(result.data)
--- App/routes.py
+++ App/routes.py
@@ -2,10 +2,10 @@
 __author__ = 'responsible'
 from App import app
 from flask_restful import Api
-from App.controller import Protected, Login, Users, Services
+from App.controller import Login, Users, Services, Roles
 
 api = Api(app, default_mediatype="application/json")
 api.add_resource(Users, '/users')
 api.add_resource(Services, '/services')
 api.add_resource(Login, '/login')
-api.add_resource(Protected, '/protected')
+api.add_resource(Roles, '/roles')
